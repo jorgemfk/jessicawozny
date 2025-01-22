2b92e9b8ce9e45d5dcc78f89960e4d9d50e901be
Jorge Luis Martinez Ramirez <jorge_mfk@hotmail.com>
2025-01-22 09:58:20 -0600

    whitenoise
    whitenoise

--- render_deploy/settings.py
+++ render_deploy/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+if not DEBUG:
+    # Define la ruta donde WhiteNoise guardará los archivos estáticos después de `collectstatic`
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    # Si usas archivos estáticos en la carpeta "static" del proyecto
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
