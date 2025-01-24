b60e57e7793f71bfba84315b2f9d052549ae347b
Jorge Luis Martinez Ramirez <jorge_mfk@hotmail.com>
2025-01-23 20:25:59 -0600

    imagenes
    imagenes

--- example/static/wozny/js/catalogo.js
+++ example/static/wozny/js/catalogo.js
@@ -1,7 +1,63 @@
 const obras = [
-    { nombre: "Obra A", anio: 1995, imagen: "obra1.jpg", descripcion: "Descripción de Obra A", tecnica: "Óleo sobre lienzo" },
-    { nombre: "Obra B", anio: 2001, imagen: "1.png", descripcion: "Descripción de Obra B", tecnica: "Acrílico sobre madera" },
-    { nombre: "Zbra C", anio: 1995, imagen: "obra2.jpg", descripcion: "Descripción de Obra C", tecnica: "Acrílico sobre madera" },
+    { nombre: "Herramienta madre", anio: 2020, imagen: "1.png", descripcion: "Pulpad epapelreciclado,alambre, piedraspómez,telamalla,tejidocrocheten hilometálico,tejidoscrochete nalgodón, poliéster, selladazo,rferdor esmalte de uñas, pintura vinilica, taponesde hule, globuli, pegamento,cantera", dimension: "90x75x95cm" },
+    { nombre: "Ventana Oval", anio: 2024, imagen: "2.png", descripcion: "Estructuradeherrería,esmalte, allapnat denylon, fideosdenatación,placarecortadade espuma,alambrerecocidocubiertod epapel bubruaj y papel impreso en xeróx, engrudo , yeso piedra, arleamc obcreido ,cinta para enmascarillar,vendade o,yse o,sge tapa ed co,itáspl codo de plástico, muestra d e espuma con recubrimientode lentejauetlad, e sardinasalambrerecorceicduobierto dtee l aceong rudo , laminacorrugadadorada", dimension: "240x130x140com" },
+    { nombre: "Steck portátil 2024(beige claro)", anio: 2024, imagen: "3.png", descripcion: "Escultura de viaje y escritorio(Diver) Jabonera de plastico, tejidos en algodón, poliester,alambrerecocido, arcillad epapel,esmalte de uñas", dimension: "8x11x4cm" },
+    {
+    nombre: "Retrato Madre e Hija de la serie Imitación",
+    anio: 2024,
+    descripcion: "Esmalte de uñas sobre papel.",
+    dimension: "21×30 cm",
+    imagen: "4.png"
+  },
+  {
+    nombre: "Tremor patología",
+    anio: 2023,
+    descripcion: "Herrería forrada de volumen tejido crochet en algodón, plastificado y pintado con esmalte de uñas, poliéster, esferas de vidrio, lámina galvanizada en ángulo, impresiones xerox, engrudo, vibrador con control remoto, pulpa de papel, pegamento.",
+    dimension: "100×80×50 cm",
+    imagen: "5.png"
+  },
+  {
+    nombre: "Sed interior insaciable y pequeña",
+    anio: 2023,
+    descripcion: "Tejido crochet plastificado, pintura, gesso, bolígrafo, tejido en gancho de lana, bordado sobre franela, pegamento, poliéster.",
+    dimension: "23×15×10 cm",
+    imagen: "6.png"
+  },
+  {
+    nombre: "De la serie Sesiones/Sitzungen (temazcal cabeza perdida)",
+    anio: 2022,
+    descripcion: "Tejido de gancho en hilo de algodón desteñido, zacate, caucho negro, hilo encerado, alta temperatura, barro con reducción.",
+    dimension: "32×30×8 m",
+    imagen: "7.png"
+  },
+  {
+    nombre: "Esquina L, corpúsculos y unsupported holding",
+    anio: 2022,
+    descripcion: "Pulpa de papel, encausto, tapones de hule, caucho, estopa, arcilla de papel, esmalte de uñas, fragmentos de vidrio, pegamento.",
+    dimension: "80×80×40 cm",
+    imagen: "8.png"
+  },
+  {
+    nombre: "Ventanas deformes, cosas finitas y mundanas",
+    anio: 2013,
+    descripcion: "2 proyectores análogos hechizos, pantallas de poliestireno, empotradas en estructura de alambre recubierto, mesas hechas a la medida, plastilina, estopa, madera, alambre forrado y pintado, poliestireno expandido, estopa, dos cajas de madera con transparencias, impresión láser sobre acetato: Repertorio y Transcripción (el custodio elegirá las transparencias en su turno).",
+    dimension: "400×400×170 cm",
+    imagen: "9.png"
+  },
+  {
+    nombre: "Partial reconstruction (Jumano)",
+    anio: 2014,
+    descripcion: "Plástico de pintor, tejidos de gancho plastificados, intervenidos, incrustaciones de piedras y residuos plásticos, pintados, de materiales encontrados, base giratoria, alambre, papel maché, pintura.",
+    dimension: "200×200×350 cm",
+    imagen: "10.png"
+  },
+  {
+    nombre: "Culi intarsia",
+    anio: 2013,
+    descripcion: "Hecho de papel maché pintado, escritorio, foco, acrílico, borradores, metal, extensión.",
+    dimension: "220×38×50 cm",
+    imagen: "11.png"
+  }
 ];
 
 function cargarLista(orden) {
@@ -13,7 +69,7 @@ function cargarLista(orden) {
     let obrasOrdenadas = [...obras];
 
     if (orden === "cronologico") {
-        obrasOrdenadas.sort((a, b) => a.anio - b.anio);
+        obrasOrdenadas.sort((a, b) => b.anio - a.anio);
     } else {
         obrasOrdenadas.sort((a, b) => a.nombre.localeCompare(b.nombre));
     }
@@ -57,7 +113,9 @@ function mostrarObra(obra) {
     document.getElementById("titulo-obra").textContent = obra.nombre;
     document.getElementById("descripcion").textContent = obra.descripcion;
     document.getElementById("anio").textContent = obra.anio;
-    document.getElementById("tecnica").textContent = obra.tecnica;
+    document.getElementById("dimension").textContent = obra.dimension;
+    document.querySelectorAll("lista-obras li").forEach(li => li.classList.remove("seleccionado"));
+    event.target.classList.add("seleccionado");
 }
 
 function ordenar(criterio) {
